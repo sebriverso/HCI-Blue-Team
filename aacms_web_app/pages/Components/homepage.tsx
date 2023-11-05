34d480abf91322749d290dbf071c3eeab264d22b
Cameron Schmidt <cschmidt@seic.com>
2023-11-05 12:28:53 -0500

    Update homepage.tsx
    update

--- aacms_web_app/pages/Components/homepage.tsx
+++ aacms_web_app/pages/Components/homepage.tsx
@@ -6,14 +6,14 @@ const HomePage = () => {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', paddingTop: '40px', paddingBottom: '40px' }}>
       {/* Slideshow Section */}
-      <div style={{ flex: '2', maxWidth: '600px', paddingRight: '20px'}}>
+      <div style={{ flex: '2', maxWidth: '500px', marginRight: '30px'}}>
         <Slideshow />
       </div>
 
       {/* Main Content Section */}
-      <div style={{ flex: '3', maxWidth: '500px', paddingRight: '20px' }}>
-        <h2 style={{ color: '#4434A6', fontSize: '40px', fontFamily: 'Sanchez',fontWeight: 'bold', marginBottom: '8px', textAlign: 'center' }}>Arts Academy Charter Middle School</h2>
-        <p style={{ color: '#4434A6', fontSize: '20px', fontFamily: 'Sanchez',fontWeight: 'bold', marginBottom: '16px' }}>
+      <div style={{ flex: '3', maxWidth: '600px', paddingRight: '20px' }}>
+        <h2 style={{ color: '#4434A6', fontSize: '50px', fontFamily: 'Sanchez',fontWeight: 'bold', marginBottom: '8px', textAlign: 'center' }}>Arts Academy Charter Middle School</h2>
+        <p style={{ color: '#4434A6', fontSize: '30px', fontFamily: 'Sanchez',fontWeight: 'bold', marginBottom: '16px' }}>
           AACMS is an arts-focused public middle school for students in grades five through eight with a passion for the arts. Any Pennsylvania student is eligible to enroll via an application and interview process. Learn more about how your child can join our community of student artists below!
         </p>
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
@@ -30,8 +30,8 @@ const HomePage = () => {
       </div>
 
       {/* Quick Navigation Section */}
-      <div style={{ flex: '2', maxWidth: '200px', paddingLeft: '20px' }}>
-        <h2 style={{ color: '#4434A6', fontSize: '20px', fontFamily: 'Sanchez',fontWeight: 'bold', marginBottom: '8px' }}>Quick Navigation</h2>
+      <div style={{ flex: '2', maxWidth: '300px', paddingLeft: '20px' }}>
+        <h2 style={{ color: '#4434A6', fontSize: '30px', fontFamily: 'Sanchez',fontWeight: 'bold', marginBottom: '8px' }}>Quick Navigation</h2>
         <div>
           <button style={{ backgroundColor: '#4434A6', color: '#FFFFFF', width: '100%',fontFamily: 'Sanchez',fontWeight: 'bold', padding: '8px 16px', border: 'none', borderRadius: '4px', marginBottom: '20px', cursor: 'pointer' }}>
             Button 1
