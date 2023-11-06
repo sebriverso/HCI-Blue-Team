d5895e42873129af498cbb3359a24f387f497012
Cameron Schmidt <cschmidt@seic.com>
2023-11-06 13:14:12 -0500

    Update test.tsx
    update

--- aacms_web_app/pages/test.tsx
+++ aacms_web_app/pages/test.tsx
@@ -2,13 +2,24 @@
 
 // Import React and Next.js components
 import React from 'react';
+import { Header } from './components/nav_button';
+import ContentPage from './Components/ContentPage';
+import Footer from './Components/footer';
 
 // Define a functional component
 const SamplePage: React.FC = () => {
+
+  const destinations = [
+    { id: 'School_Supplies', title: 'School Supplies' },
+    { id: 'destination2', title: 'Destination 2' },
+    { id: 'destination3', title: 'Destination 3' },
+  ];
+  
   return (
     <div>
-      <h1>Hello from TypeScript Page</h1>
-      <p>This is a sample TypeScript page in Next.js.</p>
+      <Header text="My Website" />
+      <ContentPage destinations={destinations}></ContentPage>
+      <Footer></Footer>
     </div>
   );
 };
