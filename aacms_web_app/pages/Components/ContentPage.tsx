788da6e14f72736e485fffce29c5dca23e41f878
Katie Lansing <katie.a.lansing@gmail.com>
2023-11-08 09:54:57 -0500

    Handling dup Components folders

--- aacms_web_app/pages/Components/ContentPage.tsx
+++ aacms_web_app/pages/Components/ContentPage.tsx
@@ -39,15 +39,15 @@ const ContentPage: React.FC<ContentPageProps> = ({ destinations }) => {
         padding: '10px 0',color: '#4434A6', fontSize: '40px', fontFamily: 'Sanchez',fontWeight: 'bold' }}>School Supplies</h2>
     
       <p style= {{color: '#4434A6', fontSize: '24px', fontFamily: 'League Spartan'}}>To help students, parents, and families prepare for classes, this page provides lists of required and recommended school supplies for each grade level. 
-    Having the necessary supplies and materials for instruction each day is crucial for our students' academic and artistic success.
-    Last-minute changes and additions may not be shown on this page immediately so it is always best to double-check with your child's teachers. 
+    Having the necessary supplies and materials for instruction each day is crucial for our students&apos; academic and artistic success.
+    Last-minute changes and additions may not be shown on this page immediately so it is always best to double-check with your child&apos;s teachers. 
     Our teachers regularly communicate updated supply needs with parents/guardians via email or paper notice.
-    <br></br><br></br>The supply lists on this page are for academic supplies. Your child's arts teacher(s) will send home a supply list when school starts.</p>
+    <br></br><br></br>The supply lists on this page are for academic supplies. Your child&apos;s arts teacher(s) will send home a supply list when school starts.</p>
 <h2 style={{
     textAlign: 'center',
     padding: '10px 0',color: '#4434A6', fontSize: '40px', fontFamily: 'Sanchez',fontWeight: 'bold' }}>Supplies by Grade Level</h2>
 
-<p style= {{color: '#4434A6', fontSize: '24px', fontFamily: 'League Spartan'}}>Please contact your child's teacher or the AACMS Main Office with any questions about these school supply lists.</p>
+<p style= {{color: '#4434A6', fontSize: '24px', fontFamily: 'League Spartan'}}>Please contact your child&apos;s teacher or the AACMS Main Office with any questions about these school supply lists.</p>
 <div style={{ display: 'flex', justifyContent: 'space-between' }}>
       <div style={{
         width: '70%',
@@ -64,7 +64,7 @@ const ContentPage: React.FC<ContentPageProps> = ({ destinations }) => {
 
       <div style={{ height: '30%', paddingRight: '0px' }}>
         <p style={{ color: '#4434A6', fontSize: '24px', fontFamily: 'League Spartan' }}>
-        If you are able, Please consider donating any of the below items to each of your child's classrooms. These community supplies are used by all students and depleted frequently. Thank you!
+        If you are able, Please consider donating any of the below items to each of your child&apos;s classrooms. These community supplies are used by all students and depleted frequently. Thank you!
         </p>
         <p style={{ color: '#4434A6', fontSize: '24px', fontFamily: 'League Spartan', textAlign: "center" }}>
         <br></br>Tissue Boxes
